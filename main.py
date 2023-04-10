b40679ed53e91e9fc2876f95497f5c5749d4cac2
alexey <125185084+tsaritsennko@users.noreply.github.com>
2023-04-10 22:55:01 +0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,28 @@
-import Parser
 
-if __name__ == '__main__':
-    Parser.parse()
+from bs4 import BeautifulSoup  # импортируем библиотеку BeautifulSoup
+import requests  # импортируем библиотеку requests
+import openpyxl # импортируем библиотеку openpyxl
+
+def parse():
+    headers = {'User-Agent': 'Mozilla/5.0'} # этот заголовок позволит нам парсить динамическую страницу
+    url = 'https://omsk.hh.ru/search/vacancy?text=Python&from=suggest_post&salary=&ored_clusters=true'  # передаем необходимый URL адрес
+    page = requests.get(url, headers=headers)  # отправляем запрос методом Get на данный адрес и получаем ответ в переменную
+    print(page.status_code) # смотрим ответ
+    soup = BeautifulSoup(page.text, "html.parser")  # передаем страницу в bs4
+    block = soup.findAll('h3', class_='bloko-header-section-3')  # находим контейнер с нужным классом
+
+    wb = openpyxl.load_workbook(filename='results.xlsx') # открываем файл
+    worksheet = wb['Result'] # открываем нужную страницу
+
+    vacancy = []
+
+    for data in block:  # проходим циклом по содержимому контейнера
+        if data.find('a', class_='serp-item__title') is not None:  # находим тег <a>
+            vacancy.append(data.text)
+
+    worksheet['A1'] = "Название вакансии"
+    for item in vacancy:
+        worksheet.append([item])
+
+    print('Successful!')
+    wb.save('results.xlsx')
